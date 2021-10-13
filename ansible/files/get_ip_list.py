d82ea4a6c81499e2e59d5b602890992504f562f6
vlyulin <vlyulin@gmal.com>
2021-10-13 13:33:42 +0300

    Add ansible-1

---
+++ ansible/files/get_ip_list.py
@@ -0,0 +1,19 @@
+import sys, json
+import fnmatch
+import objectpath
+
+if len(sys.argv) == 1:
+    print("{}")
+    exit;
+
+grps = dict.fromkeys(sys.argv[1:],[])
+
+json_data = json.load(sys.stdin)
+tree = objectpath.Tree(json_data)
+
+for key in grps.keys():
+    ips = tree.execute('$.*[@.labels.tags is "' + key + '"].network_interfaces..primary_v4_address.one_to_one_nat.address')
+    ipsl = list(ips)
+    grps[key] = {"hosts": ipsl}
+
+print(json.dumps(grps, indent=4, sort_keys=True))
