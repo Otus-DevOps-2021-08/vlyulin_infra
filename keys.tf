5a5218d6ff6aa0e7c4d9fb433835e2ece8c99d4b
vlyulin <vlyulin@gmal.com>
2021-10-28 22:52:11 +0300

    Add ansible-3

--- keys.tf
+++
@@ -1,8 +0,0 @@
-Key ID:
-QpyXJS4Dpw8H4v6BLL8W
-Your secret key:
-gpqtcoi7XYMvN1dyneRWQaOVIT5iT22OZn8QwKZS
-Save the key and the ID. After you close this window, you won’t be able to access the key value.
-
-Organization Token = 0RlWmyxWVp9PYA.atlasv1.eW6CmsgXyJbzyDoHKcOQE9azq5tEjppMQwAOHyt6mqMQhszgqViIdzvpn7hssNnyJaY
-API token = OKg3xVA1tcxtcA.atlasv1.QPD0Dx2xTUIw8EJpaW4szKFVfywVa3dRAXz3DEb9OkYdbxXoiOyRUY8y4VNgsYxk2Qc
