62e625f415b596d658ccc368be1e4e5c16af32fd
vlyulin <vlyulin@gmal.com>
2021-10-28 22:19:33 +0300

    Add ansible-3

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,4 +1,4 @@
-provider "yandex" {
+provider "yandex-cloud/yandex" {
   service_account_key_file = var.service_account_key_file
   cloud_id                 = var.cloud_id
   folder_id                = var.folder_id
